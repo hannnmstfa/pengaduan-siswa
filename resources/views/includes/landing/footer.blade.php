9116beae77ec0a843166c25ec94fb2ec55de0960
hannnmstfa <burhanmusthofa1@gmail.com>
2026-06-24 16:09:36 +0700

    first-commit

---
+++ resources/views/includes/landing/footer.blade.php
@@ -0,0 +1,7 @@
+<!-- ======= Footer ======= -->
+<footer id="footer">
+  <div class="container">
+    <div class="text-center">
+      © 2026 | By <strong>Pengaduan Siswa</strong>
+    </div>
+</footer><!-- End Footer -->
