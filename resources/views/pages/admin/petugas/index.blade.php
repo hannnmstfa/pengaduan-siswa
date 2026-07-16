fe6bc75ca55e26afc85802f6e80a6d2636bfdb6e
Cobood <ssyifa087@gmail.com>
2026-07-16 12:50:34 +0700

    fix tamabh penugas

--- resources/views/pages/admin/petugas/index.blade.php
+++ resources/views/pages/admin/petugas/index.blade.php
@@ -12,10 +12,11 @@
       </h2>
 
       <div class="my-4 mb-6">
-                < href="{{ route('petugas.create')}} "
+                <a href="{{ route('petugas.create')}} "
                   class="px-5 py-3  font-medium leading-5 text-white transition-colors duration-150 bg-red-600 border border-transparent rounded-lg active:bg-red-600 hover:bg-red-700 focus:outline-none focus:shadow-outline-red">
                   Tambah Petugas
 
+                  </a>
           </div>
       <div class="w-full mb-8 overflow-hidden rounded-lg shadow-xs">
         <div class="w-full overflow-x-auto">
