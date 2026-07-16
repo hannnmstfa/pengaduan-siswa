09e676d5cb679bbbd54095d5fbc93614d111bf5e
Cobood <ssyifa087@gmail.com>
2026-07-16 13:36:33 +0700

    eror

--- resources/views/pages/admin/pengaduan/detail.blade.php
+++ resources/views/pages/admin/pengaduan/detail.blade.php
@@ -8,7 +8,7 @@
   <main class="h-full pb-16 overflow-y-auto">
     <div class="container grid px-6 mx-auto">
       <h2 class="my-6 text-2xl font-semibold text-center text-gray-700 dark:text-gray-200">
-        Detail Pengaduanaaa
+        Detail Pengaduan
       </h2>
 
 
