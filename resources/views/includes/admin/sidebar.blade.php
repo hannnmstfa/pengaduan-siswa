7a65efa33c7833f7af7c67dcd6de84e43f52f17b
Cobood <ssyifa087@gmail.com>
2026-08-19 09:52:41 +0700

    selesai

--- resources/views/includes/admin/sidebar.blade.php
+++ resources/views/includes/admin/sidebar.blade.php
@@ -87,7 +87,22 @@
         </a>
       </li>
     </ul>
-
+<ul class="mt-6">
+      <li class="relative px-6 py-3">
+        <span
+          class="{{ (request()->routeIs('dashboard')) ? 'absolute inset-y-0 left-0 w-1 bg-red-600 rounded-tr-lg rounded-br-lg' : '' }} "
+          aria-hidden="true"></span>
+        <a class="inline-flex items-center w-full text-sm font-semibold  transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200"
+          href="{{ route('dashboard')}} ">
+          <svg class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+            <path
+              d="M10.707 2.293a1 1 0 00-1.414 0l-7 7a1 1 0 001.414 1.414L4 10.414V17a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 001 1h2a1 1 0 001-1v-6.586l.293.293a1 1 0 001.414-1.414l-7-7z" />
+          </svg>
+          <span class="ml-4">Selesai</span>
+        </a>
+      </li>
+    </ul>
+    <ul></ul>
   </div>
 </aside>
 <!-- Mobile sidebar -->
