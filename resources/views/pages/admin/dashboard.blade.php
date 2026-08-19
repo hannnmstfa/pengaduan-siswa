1798800c3bbc9421ee8eb9d6f59f6a4edbeddbeb
Cobood <ssyifa087@gmail.com>
2026-08-19 10:03:55 +0700

    selesaiii

--- resources/views/pages/admin/dashboard.blade.php
+++ resources/views/pages/admin/dashboard.blade.php
@@ -151,7 +151,7 @@
             </div>
             <div>
               <p class="mb-2 text-sm font-medium text-gray-600 dark:text-gray-400">
-                Selesai laporan
+                Selesai
               </p>
               <p class="text-lg font-semibold text-gray-700 dark:text-gray-200">
                 {{ $tanggapan }}
