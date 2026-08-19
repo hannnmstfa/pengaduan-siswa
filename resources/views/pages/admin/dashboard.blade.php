fa9d0356d15127f1a25a788a5159e6879b43af5c
Cobood <ssyifa087@gmail.com>
2026-08-19 10:03:09 +0700

    selaisa11

--- resources/views/pages/admin/dashboard.blade.php
+++ resources/views/pages/admin/dashboard.blade.php
@@ -151,14 +151,6 @@
             </div>
             <div>
               <p class="mb-2 text-sm font-medium text-gray-600 dark:text-gray-400">
-                Jumlah Tanggapan
-              </p>
-              <p class="text-lg font-semibold text-gray-700 dark:text-gray-200">
-                {{ $tanggapan }}
-              </p>
-            </div>
-            <div>
-              <p class="mb-2 text-sm font-medium text-gray-600 dark:text-gray-400">
                 Selesai laporan
               </p>
               <p class="text-lg font-semibold text-gray-700 dark:text-gray-200">
